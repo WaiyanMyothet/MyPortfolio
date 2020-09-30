b635f4bf37858f5ec4bb82ec02b5c9e858ed0f28
Waiyan Myothet <menooby1996@gmail.com>
2020-09-30 21:11:13 +0630

    adding cache

--- sw.js
+++ sw.js
@@ -1,14 +1,44 @@
-self.addEventListener('install', event => {
-    console.log('Service worker installing...');
-    self.skipWaiting();
-  });
-  
-  self.addEventListener('activate', event => {
-    console.log('Service worker activating...');
-  });
-  
-  // I'm a new service worker
-  
-  self.addEventListener('fetch', event => {
-    console.log('Fetching:', event.request.url);
-  });
+const filesToCache = [
+  "/",
+  "css/style.css",
+  "image/utycc.webp",
+  "image/waiyan.jpg",
+  "image/yammobots.png",
+  "image/favicon.ico",
+  "index.html",
+];
+
+const staticCacheName = "cache-v1";
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(staticCacheName).then((cache) => {
+      return cache.addAll(filesToCache);
+    })
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", (event) => {
+  const cacheWhitelist = [staticCacheName];
+
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cacheName) => {
+          if (cacheWhitelist.indexOf(cacheName) === -1) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
+
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    fetch(event.request).catch(function () {
+      return caches.match(event.request);
+    })
+  );
+});
